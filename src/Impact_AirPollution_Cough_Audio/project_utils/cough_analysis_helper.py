432620332798e22f630a3b6550cd826f1f95d94b
Vinod Cherian <vinodcherian5@gmail.com>
2023-07-17 23:31:50 +0530

    Update cough_analysis_helper.py

--- src/Impact_AirPollution_Cough_Audio/project_utils/cough_analysis_helper.py
+++ src/Impact_AirPollution_Cough_Audio/project_utils/cough_analysis_helper.py
@@ -30,14 +30,14 @@ def detect_cough_from_model(model, mfcc, mels, zcr, sc, sr):
     extracted_features=[]
     extracted_features.append([mfcc[0], mfcc[1], mfcc[2], mfcc[3], mfcc[4], mfcc[5], mfcc[6], mfcc[7], mfcc[8], mfcc[9], mfcc[10], mfcc[11], mfcc[12], mfcc[13], mfcc[14], mfcc[15], mels[0], mels[1], mels[2], mels[3], mels[4], mels[5], mels[6], mels[7], mels[8], mels[9], zcr[0], sc[0], sr[0]])
     extracted_features_df = pd.DataFrame(extracted_features, columns = ['mfcc1', 'mfcc2', 'mfcc3', 'mfcc4', 'mfcc5', 'mfcc6', 'mfcc7', 'mfcc8', 'mfcc9', 'mfcc10', 'mfcc11', 'mfcc12', 'mfcc13', 'mfcc14', 'mfcc15', 'mfcc16', 'mels1', 'mels2', 'mels3',  'mels4', 'mels5', 'mels6', 'mels7', 'mels8', 'mels9', 'mels10', 'zcr', 'sc', 'sr'])
-    #prediction_result = model.predict(extracted_features_df)
-    prediction_result = predict_model(model,data=extracted_features_df)
+    prediction_result = model.predict(extracted_features_df)[0]
+    #prediction_result = predict_model(model,data=extracted_features_df)
     return prediction_result
 
-def detect_cough_severity_from_model(model, mfcc, mels, zcr, sc, sr, audio_sr, audio_file):
+def detect_cough_severity_from_model(model, mfcc, melspec, zcr, sc, sr, audio_sr, audio_file):
     extracted_features=[]
     chroma = chroma_scaled_features(audio_sr, audio_file)
     extracted_features.append([mfcc[0], mfcc[1], mfcc[2], mfcc[3], mfcc[4], mfcc[5], mfcc[6], mfcc[7], mfcc[8], mfcc[9], mfcc[10], mfcc[11], mfcc[12], mfcc[13], mfcc[14], mfcc[15], melspec[0], melspec[1], melspec[2], melspec[3], melspec[4], melspec[5], melspec[6], melspec[7], melspec[8], melspec[9], zcr[0], sc[0], sr[0], chroma[0]])
     extracted_features_df = pd.DataFrame(extracted_features, columns = ['mfcc1', 'mfcc2', 'mfcc3', 'mfcc4', 'mfcc5', 'mfcc6', 'mfcc7', 'mfcc8', 'mfcc9', 'mfcc10', 'mfcc11', 'mfcc12', 'mfcc13', 'mfcc14', 'mfcc15', 'mfcc16', 'melspec1', 'melspec2', 'melspec3', 'melspec4', 'melspec5', 'melspec6', 'melspec7', 'melspec8', 'melspec9', 'melspec10', 'zcr', 'sc', 'sr', 'chroma'])
-    prediction_result = model.predict(extracted_features_df)
+    prediction_result = model.predict(extracted_features_df)[0]
     return prediction_result
