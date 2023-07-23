0c88c871880610d2b66e890d9e058dbd0da8173f
HARSHITA <harshitabaghel2005@gmail.com>
2023-07-23 22:31:37 +0530

    remove unwanted css in  menu.py

--- src/Impact_AirPollution_Cough_Audio/project_utils/menu.py
+++ src/Impact_AirPollution_Cough_Audio/project_utils/menu.py
@@ -28,9 +28,7 @@ def load_css():
   }
   h4 {
     font-size: 19px;
-    line-height: 1.375em;
     color: #303030;
-    font-weight: 400;
     margin-bottom: 30px;
   }  
   p {
@@ -51,34 +49,6 @@ def load_css():
   .bg-grey {
     background-color: #f6f6f6;
   }
-  .logo-small {
-    color: #f4511e;
-    font-size: 50px;
-  }
-  .logo {
-    color: #f4511e;
-    font-size: 200px;
-  }
-  .thumbnail {
-    padding: 0 0 15px 0;
-    border: none;
-    border-radius: 0;
-  }
-  .thumbnail img {
-    width: 100%;
-    height: 100%;
-    margin-bottom: 10px;
-  }
-  .carousel-control.right, .carousel-control.left {
-    background-image: none;
-    color: #f4511e;
-  }
-  .carousel-indicators li {
-    border-color: #f4511e;
-  }
-  .carousel-indicators li.active {
-    background-color: #f4511e;
-  }
   .item h4 {
     font-size: 19px;
     line-height: 1.375em;
@@ -86,56 +56,13 @@ def load_css():
     font-style: italic;
     margin: 70px 0;
   }
-  .item span {
-    font-style: normal;
-  }
-  .panel {
-    border: 1px solid #f4511e; 
-    border-radius:0 !important;
-    transition: box-shadow 0.5s;
-  }
-  .panel:hover {
-    box-shadow: 5px 0px 40px rgba(0,0,0, .2);
-  }
-  .panel-footer .btn:hover {
-    border: 1px solid #f4511e;
-    background-color: #fff !important;
-    color: #f4511e;
-  }
-  .panel-heading {
-    color: #fff !important;
-    background-color: #f4511e !important;
-    padding: 25px;
-    border-bottom: 1px solid transparent;
-    border-top-left-radius: 0px;
-    border-top-right-radius: 0px;
-    border-bottom-left-radius: 0px;
-    border-bottom-right-radius: 0px;
-  }
-  .panel-footer {
-    background-color: white !important;
-  }
-  .panel-footer h3 {
-    font-size: 32px;
-  }
-  .panel-footer h4 {
-    color: #aaa;
-    font-size: 14px;
-  }
-  .panel-footer .btn {
-    margin: 15px 0;
-    background-color: #f4511e;
-    color: #fff;
-  }
   .navbar {
-    margin-bottom: 0;
     background-color: #f4511e;
     z-index: 9999;
     border: 0;
     font-size: 12px !important;
     line-height: 1.42857143 !important;
     letter-spacing: 4px;
-    border-radius: 0;
     font-family: Montserrat, sans-serif;
     font-weight: 600;
   }
@@ -151,12 +78,6 @@ def load_css():
     border-color: transparent;
     color: #fff !important;
   }
-  footer .glyphicon {
-    font-size: 20px;
-    margin-bottom: 20px;
-    color: #f4511e;
-  }
-  .slideanim { visibility:hidden;}
   .slide {
     animation-name: slide;
     -webkit-animation-name: slide;
@@ -192,11 +113,6 @@ def load_css():
     .btn-lg {
       width: 100%;
       margin-bottom: 35px;
-    }
-  }
-  @media screen and (max-width: 480px) {
-    .logo {
-      font-size: 150px;
     }
   }
   </style>
